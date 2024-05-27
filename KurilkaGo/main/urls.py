fb26ac63fd5d0fbae1141b93030f9e48208760b8
Hlib Khoruznhyi <gleb5192005@gmail.com>
2024-05-27 17:59:43 +0200

    added links and urls

--- KurilkaGo/main/urls.py
+++ KurilkaGo/main/urls.py
@@ -3,7 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('about', views.about),
-    path('cigarettes/<int:page>',views.cigarettes),
-    path('cigarette/<int:id>', views.cigarette)
+    path('cigarettes/<int:page>', views.getCigarettes,  name='get_cigarettes'),
+    path('cigarette/<int:id>', views.getCigarette),
+    path('disposablevapes/<int:page>', views.getDisposableVapes),
+    path('disposablevape/<int:id>', views.getDisposableVape),
+    path('hookahs/<int:page>', views.getHookahs),
+    path('hookah/<int:id>', views.getHookah),
+    path('iqoses/<int:page>', views.getIqoses),
+    path('iqos/<int:id>', views.getIqos),
+    path('liquids/<int:page>', views.getLiquids),
+    path('liquid/<int:id>', views.getLiquid),
+    path('about', views.about)
 ]
--- KurilkaGo/main/views.py
+++ KurilkaGo/main/views.py
@@ -1,19 +1,132 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'homepage.html')
 
+
 def about(request):
     return render(request, 'about.html')
 
-def cigarettes(request, page):
-    #cigarettes = Cigarette.objects.all()
-    cigarettes_page = Cigarette.objects.all()[(page - 1) * 12: page * 12]
-    return render(request, 'products-page.html', {"products": cigarettes_page})
 
-def cigarette(request, id):
+def getCigarettes(request, page):
+
+    # cigarettes_page = Cigarette.objects.all()[(page - 1) * 12: page * 12]
+    cigarettes_page = [
+        {
+            'id': 1,
+            'title': 'Malborro',
+            'price': 10
+        },
+        {
+            'id': 2,
+            'title': 'Parlage',
+            'price': 23
+        },
+        {
+            'id': 3,
+            'title': 'fawe',
+            'price': 23
+        },
+        {
+            'id': 4,
+            'title': 'Pafadrlage',
+            'price': 23
+        },
+    ]
+    return render(request, 'products-page.html', {
+        'title': 'Cigarettes',
+        'product_url': '/cigarette/',
+        "products": cigarettes_page,
+    })
+
+
+def getCigarette(request, id):
     this_cigarette = get_object_or_404(Cigarette, id=id)
     return render(request, 'item-page.html', {"product": this_cigarette})
+
+
+def getDisposableVapes(request, page):
+
+    #disposable_vape_page = DisposableVape.objects.all()[(page - 1) * 12: page * 12]
+    disposable_vape_page = [
+        {
+            'id': 1,
+            'title': 'Malborro',
+            'price': 10
+        },
+        {
+            'id': 2,
+            'title': 'Parlage',
+            'price': 23
+        },
+        {
+            'id': 3,
+            'title': 'fawe',
+            'price': 23
+        },
+        {
+            'id': 4,
+            'title': 'Pafadrlage',
+            'price': 23
+        },
+    ]
+
+    return render(request, 'products-page.html', {
+        "title": 'E-Cig',
+        'product_url': '/disposablevape/',
+        "products": disposable_vape_page
+    })
+
+
+def getDisposableVape(request, id):
+    this_disposable_vape = get_object_or_404(DisposableVape, id=id)
+    return render(request, 'item-page.html', {"product": this_disposable_vape})
+
+
+def getHookahs(request, page):
+
+    hookahs_page = Hookah.objects.all()[(page - 1) * 12: page * 12]
+    return render(request, 'products-page.html', {
+        'title': 'Hookah',
+        'product_url': '/hookah/',
+        "products": hookahs_page,
+    })
+
+
+def getHookah(request, id):
+    this_hookah = get_object_or_404(Hookah, id=id)
+    return render(request, 'item-page.html', {"product": this_hookah})
+
+
+def getIqoses(request, page):
+
+    iqos_page = Heating.objects.all()[(page - 1) * 12: page * 12]
+    return render(request, 'products-page.html', {
+        'title': 'Iqos',
+        'product_url': '/iqos/',
+        "products": iqos_page,
+    })
+
+
+def getIqos(request, id):
+    this_iqos = get_object_or_404(Heating, id=id)
+    return render(request, 'item-page.html', {"product": this_iqos})
+
+
+def getLiquids(request, page):
+
+    liquid_page = Liquid.objects.all()[(page - 1) * 12: page * 12]
+    return render(request, 'products-page.html', {
+        'title': 'Liquid',
+        'product_url': '/liquid/',
+        "products": liquid_page,
+    })
+
+
+def getLiquid(request, id):
+    this_liquid = get_object_or_404(Liquid, id=id)
+    return render(request, 'item-page.html', {"product": this_liquid})
